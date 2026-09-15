5c270664d3d38e28f25ef32b4e120eb083985055
thelifemaurya <thelifemaurya@gmail.com>
2026-09-15 09:05:13 +0530

    Add Google AdSense site script

---
+++ components/adsense-script.tsx
@@ -0,0 +1,14 @@
+import Script from 'next/script'
+
+const ADSENSE_CLIENT = 'ca-pub-1101292617046429'
+
+export default function AdSenseScript() {
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  )
+}
